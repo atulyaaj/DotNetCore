45f7d668d06b6a48a3a307a0b9e3bf1423e575e0
Atulya Jaiswal <atulyaaj6@gmail.com>
2022-11-07 22:15:05 +0530

    Atulya added

---
+++ CodeFirst/CodeFirst/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CodeFirst
+{
+    [Table("courses")]
+    public class Course
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string CourseName { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+
+namespace CodeFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new SchoolDbContext();
+            var std = new Student()
+            {
+                StudentName = "Amit",
+                Contact = 1234567891
+
+            };
+
+            context.students.Add(std);
+            context.SaveChanges();
+            Console.WriteLine("Added Successfully.....");
+            Console.Read();
+
+            var std1 = context.students.Find(3);
+            context.Entry(std1).State = EntityState.Deleted;
+            context.SaveChanges();
+            Console.Read();
+
+            Console.WriteLine("Atulya Added Code.......");
+
+        }
+    }
+}
